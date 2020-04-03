d5db019b47700cdcb55ae07d83a9057d6204205b
Levan1199 <61540906+Levan1199@users.noreply.github.com>
2020-04-03 12:04:02 +0700

    Add files via upload

---
+++ import.py
@@ -0,0 +1,33 @@
+import csv
+import os
+
+from flask import Flask, session, render_template
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+def func():
+    f = open("books.csv")
+    reader = csv.reader(f)    
+    
+    x = 0
+    for isbn, title, author, year in reader:         
+        print("uploading: " + x)
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+        {"isbn": isbn, "title": title, "author": author, "year": int(year)})
+        db.commit()   
+        x += 1                  
